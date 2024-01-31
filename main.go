0a7c0a33e82870a431058de54def4dbf949ac1ca
John Currado <jjcurrado@gmail.com>
2024-01-31 13:43:46 -0500

    moved db stuff to helpers

--- main.go
+++ main.go
@@ -5,58 +5,11 @@ import (
 	"log"
 	"net/http"
 	"text/template"
-
-	_ "github.com/lib/pq"
 )
-
-// TODO:
-//	- un-stub the helper stuff
-//	- Add
-//		- resistances
-//		- flaws
-//		- bonds
-//		- personality traits
-//		- ideals
-//
-//	- host endpoint
-//
-//  - persistent storage
-//
-//	- Features:
-//		- create images of characters based on their sheets
-//		- add rolling functionality for each sheet
 
 func main() {
 
-	//err := godotenv.Load(".env")
-
-	//if err != nil {
-	//log.Fatal("Error loading .env file")
-	//}
-
-	//host := os.Getenv("DB_HOST")
-	//port := os.Getenv("DB_PORT")
-	//user := os.Getenv("DB_USER")
-	//password := os.Getenv("DB_PASSWORD")
-	//dbname := os.Getenv("DB_NAME")
-
-	//psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
-	//db, err := sql.Open("postgres", psqlInfo)
-
-	//if err != nil {
-	//panic(err)
-	//}
-	//defer db.Close()
-	//err = db.Ping()
-
-	//if err != nil {
-	//panic(err)
-	//}
-	//fmt.Println("Successfully connected!")
-
 	// host static files
-	// TODO: better directory structure?
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
--- utilities/helpers.go
+++ utilities/helpers.go
@@ -1,6 +1,7 @@
 package utilities
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
@@ -9,6 +10,36 @@ import (
 	"github.com/joho/godotenv"
 	openai "github.com/sashabaranov/go-openai"
 )
+
+func ConnectDB() {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected!")
+}
 
 func CreateClient() *openai.Client {
 	err := godotenv.Load(".env")
