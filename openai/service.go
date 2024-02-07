191a611e91b73cbac025550146b6de74ebae4cef
John Currado <jjcurrado@gmail.com>
2024-02-07 17:56:54 -0500

    removed excess whitespace, commenting, and logging

--- openai/service.go
+++ openai/service.go
@@ -11,44 +11,29 @@ import (
 	openai "github.com/sashabaranov/go-openai"
 )
 
-//TODO:
-// - use the ai in a more generic sense (i.e. have a function check which function calls its lookking for)
-
-// CreateCharacter
 // creates a character from the given description
-//
-// TODO:
-// - make the options dynamic for request (which parameters are required)
 func CreateCharacter(description string, options util.Options) util.Character {
-
 	char := RequestCharacterStats(description)
 
 	// get a spell list
 	if char.Spellcaster && options.Spells {
-
 		l := RequestSpellList(char)
-		fmt.Printf("\n%v\n", l)
-
 		s := GetAllSpells(l)
-
 		for _, spell := range s {
 			util.PrintSpell(spell)
 		}
 	}
+
 	return char
 }
 
-// RequestCharacterStats
 // Create the base character stats including: name, class, subclass, hp, ac, and ability scores
-// TODO: Do some of these requests in parallel?
 func RequestCharacterStats(prompt string) util.Character {
 	res := sendRequest(prompt)
-
 	char := GetCharacterResponse(res)
 	return char
 }
 
-// RequestSpellList
 // Given a character - make a list of spell names that they can cast
 func RequestSpellList(char util.Character) []string {
 
@@ -60,55 +45,48 @@ func RequestSpellList(char util.Character) []string {
 	return l
 }
 
-// RequestSpellDetails
 // Given the name of a spell, get its details including: name, level, range, duration, and description
 func RequestSpellDetails(spell string) util.Spell {
 
-	//TODO: check a database if we've already generated the spell details instead of prompting chat GPT for it
-	// 		also check the official SRD api ?
 	prompt := fmt.Sprintf("Give me the details for the spell %v", spell)
-	fmt.Printf("Creating spell details:\n%v\n", prompt)
-
 	res := sendRequest(prompt)
 	s := GetSpellDetailsResponse(res)
 
 	return s
 }
 
-// GetCharacterResponse
 // Extract character data from the openai response
 func GetCharacterResponse(res openai.ChatCompletionResponse) util.Character {
 	msg := res.Choices[0].Message
 	char := util.Character{}
 	json.Unmarshal([]byte(msg.ToolCalls[0].Function.Arguments), &char)
+
 	return char
 }
 
-// GetSpellListResponse
 // Extract spell name list from the openai response
 func GetSpellListResponse(res openai.ChatCompletionResponse) []string {
 	msg := res.Choices[0].Message
 	spell := util.SpellList{}
 	json.Unmarshal([]byte(msg.ToolCalls[0].Function.Arguments), &spell)
+
 	return spell.Spells
 }
 
-// GetSpellDetailsResponse
 // Extract spell detail data from the openai response
 func GetSpellDetailsResponse(res openai.ChatCompletionResponse) util.Spell {
 	msg := res.Choices[0].Message
 	spell := util.Spell{}
 	json.Unmarshal([]byte(msg.ToolCalls[0].Function.Arguments), &spell)
+
 	return spell
 }
 
-// GetAllSpells
 // Loop through the list of spell names and request the details for each in parallel
 func GetAllSpells(SpellNames []string) []util.Spell {
-
 	details := make([]util.Spell, len(SpellNames))
-
 	var wg sync.WaitGroup
+
 	for i := 0; i < len(SpellNames); i++ {
 		wg.Add(1)
 		go func(SpellName string, index int) {
@@ -129,7 +107,6 @@ func GetAllSpells(SpellNames []string) []util.Spell {
 	return details
 }
 
-// checkResponse
 // check for errors in the openai response
 func checkResponse(res openai.ChatCompletionResponse, err error) {
 	if err != nil {
@@ -140,11 +117,8 @@ func checkResponse(res openai.ChatCompletionResponse, err error) {
 	}
 }
 
-// sendRequest
 // send a request to the openai API and check for errors
 // return the response.
-// TODO:
-// - add rate limiter
 func sendRequest(prompt string) openai.ChatCompletionResponse {
 	// prime the dialogue for the ai
 	dialog := []openai.ChatCompletionMessage{
--- utilities/database.go
+++ utilities/database.go
@@ -13,7 +13,6 @@ import (
 var DBClient = ConnectDB()
 
 func ConnectDB() *sql.DB {
-
 	err := godotenv.Load(".env")
 
 	if err != nil {
@@ -25,9 +24,7 @@ func ConnectDB() *sql.DB {
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
-
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
 	db, err := sql.Open("postgres", psqlInfo)
 
 	if err != nil {
@@ -44,7 +41,6 @@ func ConnectDB() *sql.DB {
 
 func InsertSpell(spell Spell) (int64, error) {
 	insertStatement := "INSERT INTO spells (name, description, level, school, duration, casting_time, range, components) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)"
-
 	result, err := DBClient.Exec(insertStatement, spell.Name, spell.Description, spell.Level, spell.School, spell.Duration, spell.CastingTime, spell.Range, spell.Components)
 
 	if err != nil {
@@ -57,12 +53,10 @@ func InsertSpell(spell Spell) (int64, error) {
 		return 0, fmt.Errorf("error inserting spell: %v", err)
 	}
 
-	fmt.Printf("Inserted spell %s!", spell.Name)
 	return id, nil
 }
 
 func FindSpell(name string) (Spell, error) {
-	fmt.Printf("finding spell: %s\n", name)
 
 	row := DBClient.QueryRow(`SELECT name, level, duration, range, description, casting_time, Components, School FROM spells WHERE name=$1;`, name)
 	var (
@@ -82,7 +76,6 @@ func FindSpell(name string) (Spell, error) {
 		return Spell{}, err
 	}
 
-	fmt.Printf("Found spell %s!\n", Name)
 	return Spell{
 		Name:        Name,
 		Level:       Level,
